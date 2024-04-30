af89e2b7608d3c2f84c2d9a712f1f821102fcb58
Antonio L Avila <antonioavila433@gmail.com>
2024-04-30 01:35:57 -0400

    Ready for testing

--- visual_servoing/visual_servoing/cone_detector.py
+++ visual_servoing/visual_servoing/cone_detector.py
@@ -32,22 +32,26 @@ class ConeDetector(Node):
         self.image_sub = self.create_subscription(Image, "/zed/zed_node/rgb/image_rect_color", self.image_callback, 5)
         self.bridge = CvBridge() # Converts between ROS images and OpenCV Images
 
+        # TODO tune lookahead
+        self.look_ahead = 1000 # Measured in pixels
+        self.epsilon = 1e-5
         self.sensitivity = 35
         self.lower_white = np.array([0,0,255-self.sensitivity])
         self.upper_white = np.array([255,self.sensitivity,255])
 
         self.get_logger().info("Cone Detector Initialized")
 
-    def image_callback(self, image_msg):        
+    def image_callback(self, image_msg):
         img = self.bridge.imgmsg_to_cv2(image_msg, "bgr8")
-        # In case we want only a portion of the image
-        # y, _, _ = img.shape
-        # img[0:y//2,:,:] = 0
-        # img[4*y//5:y,:,:] = 0
-        u, v = self.get_point(img)
+        y, _, _ = img.shape
+        # TODO find a good crop
+        img[0:y//2] = 0
+        img[4*y//5:y] = 0
+        debug_img, u, v = self.get_point(img, self.lower_white, self.upper_white, self.epsilon)
+        v = self.look_ahead
 
-        cv2.circle(img, (u,v), 2, (0,0,255), 2) # Circle for debug
-        debug_msg = self.bridge.cv2_to_imgmsg(img, "bgr8")
+        cv2.circle(debug_img, (u,v), 2, (0,0,255), 2)
+        debug_msg = self.bridge.cv2_to_imgmsg(debug_img, "bgr8")
         self.debug_pub.publish(debug_msg)
 
         px = ConeLocationPixel()
@@ -55,24 +59,49 @@ class ConeDetector(Node):
         px.v = v
         self.cone_pub.publish(px)
 
-    def get_point(self, img):
+    @staticmethod
+    def get_point(img, lower_white, upper_white, epsilon):
+        # Pre-processing
         hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
-        mask = cv2.inRange(hsv, self.lower_white, self.upper_white)
-        
-        edges = cv2.Canny(mask, 50, 500, 3)
-        lines = cv2.HoughLines(edges, 1, np.pi/180, 150, None, 0, 0)
+        mask = cv2.inRange(hsv, lower_white, upper_white)
+        edges = cv2.Canny(mask, 500, 1200)
+        debug_rgb = cv2.cvtColor(edges, cv2.COLOR_GRAY2BGR)
 
-        black = np.zeros(img.shape)
-        for r_theta in lines:
-            arr = np.array(r_theta[0], dtype=np.float64)
-            r, theta = arr
-            a = np.cos(theta)
-            b = np.sin(theta)
-            x0 = a*r
-            y0 = b*r
-            cv2.line(black, (int(x0 + 1000*(-b)), int(y0 + 1000*(a))), (int(x0 - 1000*(-b)), int(y0 - 1000*(a))), (0, 0, 255), 2)
+        # Work in normal polar coordinates one "distance" is 1 and one "angle" is pi/180 radians
+        lines = cv2.HoughLines(edges, 1, np.pi/180, 130)
+        r, theta = lines[:,0,0], lines[:,0,1]
+        c, s = np.cos(theta), np.sin(theta) + epsilon # Add to not divide by 0
+        m, b = -c/s, r/s
+        m_positive, b_positive = m[m > 1], b[m > 1] # Select for vertical lines
+        N_positive = m_positive.shape[0]
+        m_negative, b_negative = m[m < -1], b[m < -1] # Select for vertical lines
+        N_negative = m_negative.shape[0]
+
+        x_intersections = []
+        y_intersections = []
+        for i in range(N_positive):
+            for j in range(N_negative):
+                x = (b_positive[i] - b_negative[j]) / (m_negative[j] - m_positive[i])
+                y = m_positive[i]*x + b_positive[i]
+                x_intersections.append(x)
+                y_intersections.append(y)
+
+        # Draw positive
+        for m_,b_ in zip(m_positive, b_positive):
+            x0 = -1000
+            y0 = int(m_*x0 + b_)
+            xf = 1000
+            yf = int(m_*xf + b_)
+            cv2.line(debug_rgb, (x0,y0), (xf,yf), (0, 0, 255), 2)
+        # Draw negative lines
+        for m_,b_ in zip(m_negative, b_negative):
+            x0 = -1000
+            y0 = int(m_*x0 + b_)
+            xf = 1000
+            yf = int(m_*xf + b_)
+            cv2.line(debug_rgb, (x0,y0), (xf,yf), (0, 0, 255), 2)
         
-        return (0,0)
+        return debug_rgb, np.mean(x_intersections), np.mean(y_intersections)
 
 def main(args=None):
     rclpy.init(args=args)
